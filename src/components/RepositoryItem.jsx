77c10a8bed86e70a1a1668a3587b222893181d2a
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-01-11 18:27:04 +0200

    start exercise 10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text>{item.description}</Text>
+      <Text>{item.description}</Text>
+      <Text>{item.description}</Text>
+      <Text>{item.description}</Text>
+      <Text>{item.description}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
