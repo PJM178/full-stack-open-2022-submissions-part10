1965805aba90c36407aaa565bffb5c3a3dd4068c
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-01-11 18:32:52 +0200

    complete 10.3

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native';
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View>
-      <Text>{item.description}</Text>
-      <Text>{item.description}</Text>
-      <Text>{item.description}</Text>
-      <Text>{item.description}</Text>
-      <Text>{item.description}</Text>
+    <View key={item.id}>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
     </View>
   );
 };
