eaddfa3cd605533ba117b49fa32c2bc17cc966d9
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-02-10 11:28:23 +0200

    refactored RepositoryItem component
    use useLocation to pass the parameters

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native';
-import { useNavigate, useParams } from 'react-router-native';
+import { View, Text, StyleSheet, Pressable, ActivityIndicator } from 'react-native';
+import { Link, useNavigate, useParams, useLocation } from 'react-router-native';
 import useRepository from '../hooks/useRepository';
 import Constants from 'expo-constants';
 import { Button } from "@react-native-material/core";
@@ -53,57 +53,97 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 5,
-  }
+  },
+  activityIndicator: {
+    flex: 1/2,
+    justifyContent: 'center',
+  },
 });
 
 
 const RepositoryItem = ({ item }) => {
+  // useLocation - a way to pass parameters with navigate()
+  const openGithub = item === undefined ? true : false;
+  const rep = item === undefined ? useLocation().state : item;
+  console.log(rep);
+  console.log('state from navigate',);
   const { repositoryId } = useParams();
+  console.log(repositoryId);
   const navigate = useNavigate();
+ 
+  // const handlePress = () => {
+  //   if (item !== undefined) {
+  //     navigate(`/${item.id}`, { item: item });
+  //   }
+  // };
 
   const handlePress = () => {
-    if (item !== undefined) {
-      navigate(`/${item.id}`)
-    }
+    navigate(`/${rep.id}`, { state: rep });
   };
 
-  if (item === undefined) {
-    const { repository, error, loading } = useRepository(repositoryId);
-    if (repository) {
-      const item = repository
-      return (
-        <View testID="repositoryItem" key={item.id} style={styles.container}>
-          <Pressable onPress={handlePress}>
-          <View style={styles.item}>
-            <RepositoryItemAvatar image={item.ownerAvatarUrl} />
-            <RepositoryItemDescription name={item.fullName} description={item.description} 
-              styles={styles.descriptionComponent} language={item.language} />
-          </View>
-          <RepositoryItemStats stars={item.stargazersCount} forks={item.forksCount} 
-            reviews={item.reviewCount} rating={item.ratingAverage} style={styles} />
-          </Pressable>
-          <View style={styles.button}>
-            <Button title="Open in GitHub" onPress={() => Linking.openURL(item.url)}/>
-          </View>
+  // if (item === undefined) {
+  //   const { repository, error, loading } = useRepository(repositoryId);
+  //   if (!repository) {
+  //     return (
+  //       <View style={styles.activityIndicator}>
+  //         <ActivityIndicator size="large" color="#0000ff" />
+  //       </View>
+  //     )
+  //   } else {
+  //     const item = repository;
+  //     return (
+  //       <View testID="repositoryItem" key={item.id} style={styles.container}>
+  //         <Pressable onPress={handlePress}>
+  //         <View style={styles.item}>
+  //           <RepositoryItemAvatar image={item.ownerAvatarUrl} />
+  //           <RepositoryItemDescription name={item.fullName} description={item.description} 
+  //             styles={styles.descriptionComponent} language={item.language} />
+  //         </View>
+  //         <RepositoryItemStats stars={item.stargazersCount} forks={item.forksCount} 
+  //           reviews={item.reviewCount} rating={item.ratingAverage} style={styles} />
+  //         </Pressable>
+  //         <View style={styles.button}>
+  //           <Button title="Open in GitHub" onPress={() => Linking.openURL(item.url)}/>
+  //         </View>
           
-        </View>
-      );
-    }
-  } else {
-    return (
-      <View testID="repositoryItem" key={item.id} style={styles.container}>
-        <Pressable onPress={handlePress}>
-        <View style={styles.item}>
-          <RepositoryItemAvatar image={item.ownerAvatarUrl} />
-          <RepositoryItemDescription name={item.fullName} description={item.description} 
-            styles={styles.descriptionComponent} language={item.language} />
-        </View>
-        <RepositoryItemStats stars={item.stargazersCount} forks={item.forksCount} 
-          reviews={item.reviewCount} rating={item.ratingAverage} style={styles} />
-        </Pressable>
+  //       </View>
+  //     );
+  //   }
+  // } else {
+  //   return (
+  //     <View testID="repositoryItem" key={item.id} style={styles.container}>
+  //       <Pressable onPress={handlePress}>
+  //       <View style={styles.item}>
+  //         <RepositoryItemAvatar image={item.ownerAvatarUrl} />
+  //         <RepositoryItemDescription name={item.fullName} description={item.description} 
+  //           styles={styles.descriptionComponent} language={item.language} />
+  //       </View>
+  //       <RepositoryItemStats stars={item.stargazersCount} forks={item.forksCount} 
+  //         reviews={item.reviewCount} rating={item.ratingAverage} style={styles} />
+  //       </Pressable>
+  //     </View>
+  //   );
+  // }
+
+  return (
+    <View testID="repositoryItem" key={rep.id} style={styles.container}>
+      <Pressable onPress={handlePress}>
+      <View style={styles.item}>
+        <RepositoryItemAvatar image={rep.ownerAvatarUrl} />
+        <RepositoryItemDescription name={rep.fullName} description={rep.description} 
+          styles={styles.descriptionComponent} language={rep.language} />
       </View>
-    );
-  }
+      <RepositoryItemStats stars={rep.stargazersCount} forks={rep.forksCount} 
+        reviews={rep.reviewCount} rating={rep.ratingAverage} style={styles} />
+      </Pressable>
+      {openGithub ? 
+        <View style={styles.button}>
+          <Button title="Open in GitHub" onPress={() => Linking.openURL(item.url)}/>
+        </View> :
+        null
+      }
+    </View>
+  );
 };
 
 export default RepositoryItem;
